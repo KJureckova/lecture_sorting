4047ac66c317528c11c8eae1231fe591ee5ea5b7
Katerina Jureckova <jureckova@vut.cz>
2024-04-17 08:42:07 +0200

    Add read_data function

--- sorting.py
+++ sorting.py
@@ -1,3 +1,4 @@
+import csv
 import os
 
 
@@ -10,10 +11,20 @@ def read_data(file_name):
     """
     cwd_path = os.getcwd()
     file_path = os.path.join(cwd_path, file_name)
+    with open(file_path, mode="r") as csv_file:
+        reader = csv.DictReader(csv_file)
+        data = {}
+        for row in reader:
+            for key in row.keys():
+                if key not in data:
+                    data[key] = [int(row[key])]
+                else:
+                    data[key].append(int(row[key]))
+    return data
 
 
 def main():
-    pass
+    numbers = read_data("numbers.csv")
 
 
 if __name__ == '__main__':
